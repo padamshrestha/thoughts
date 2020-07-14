8a8c6889831ac243542aaf932bff27151d40d533
sushantkumar23 <sushantkumar23@gmail.com>
2020-07-15 00:19:47 +0530

    Adding database

--- thoughts/factory.py
+++ thoughts/factory.py
@@ -1,4 +1,9 @@
+# factory.py
+
+import os
+import flask
 from flask import Flask
+from pymongo import MongoClient
 from flask import render_template
 import requests
 import json
@@ -7,6 +12,7 @@ import json
 
 def create_app():
 
+    MONGODB_URI = os.getenv('MONGODB_URI')
     DAVINCI_URL = "https://api.openai.com/v1/engines/davinci/completions"
 
     headers = {
@@ -47,15 +53,24 @@ def create_app():
     def load_tweet(key):
 
         data['prompt'] = key_prompt.format(key) if key else default_prompt
+        ip_address = flask.request.remote_addr
 
         try:
             response = requests.post(DAVINCI_URL, headers=headers, data=json.dumps(data))
+            res = response.json()
+            tweet = res['choices'][0]['text'].strip()
+            with MongoClient(MONGODB_URI) as client:
+                db = client.get_database()
+                page_views = db['page_views']
+                page_views.insert_one({
+                    'key': key,
+                    'tweet': tweet,
+                    'ip_address': ip_address
+                })
         except Exception as e:
             print(e)
             tweet = "Oops! That did not go as planned. Try again!"
 
-        res = response.json()
-        tweet = res['choices'][0]['text'].strip()
         print(tweet)
         return render_template('quotes.html', tweet=tweet)
 
