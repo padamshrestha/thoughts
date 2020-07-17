3198c09f5fc301f0bdac47429eb8ac742cd10cb9
sushantkumar23 <sushantkumar23@gmail.com>
2020-07-17 23:41:31 +0530

    Integrating pymongo for connection issue

--- thoughts/factory.py
+++ thoughts/factory.py
@@ -3,7 +3,7 @@
 import os
 import flask
 from flask import Flask
-from pymongo import MongoClient
+from flask_pymongo import PyMongo
 from flask import render_template
 import requests
 import json
@@ -47,31 +47,40 @@ def create_app():
     }
 
     app = Flask(__name__)
+    app.config['MONGO_URI'] = MONGODB_URI
+    mongo = PyMongo(app)
 
     @app.route('/', defaults={'key': None})
     @app.route('/<key>')
     def load_tweet(key):
 
         data['prompt'] = key_prompt.format(key) if key else default_prompt
-        ip_address = flask.request.remote_addr
 
+        # Get Tweet
         try:
             response = requests.post(DAVINCI_URL, headers=headers, data=json.dumps(data))
             res = response.json()
             tweet = res['choices'][0]['text'].strip()
-            with MongoClient(MONGODB_URI) as client:
-                db = client.get_database()
-                page_views = db['page_views']
-                page_views.insert_one({
-                    'key': key,
-                    'tweet': tweet,
-                    'ip_address': ip_address
-                })
         except Exception as e:
             print(e)
             tweet = "Oops! That did not go as planned. Try again!"
-
         print(tweet)
+
+
+        # Save data
+        ip_address = flask.request.remote_addr
+        user_agent = flask.request.user_agent.string
+        try:
+            page_views = mongo.db['page_views']
+            page_views.insert_one({
+                'key': key,
+                'tweet': tweet,
+                'ip_address': ip_address,
+                'user_agent': user_agent
+            })
+        except Exception as e:
+            print(e)
+
         return render_template('quotes.html', tweet=tweet)
 
     return app
